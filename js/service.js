0cc3882dc60b6b2679c32165822a5915492bb077
hoang160102 <hoang160102@gmail.com>
2023-03-19 22:51:36 +0700

    update

--- js/service.js
+++ js/service.js
@@ -1,19 +1,3 @@
-let sliderIndex = 0
-let getSlide = document.getElementsByClassName('slider-img')
-showSlides()
-function showSlides() {
-    let i
-    for (i = 0; i < getSlide.length; i++) {
-        getSlide[i].style.display = 'none'
-    }
-    sliderIndex++
-    if (sliderIndex > getSlide.length) {
-        sliderIndex = 1
-    }
-    getSlide[sliderIndex-1].style.display = 'block'
-    setTimeout(showSlides, 3000)
-}
-
 window.onscroll = function() {
     stickyNavbar()
 }
